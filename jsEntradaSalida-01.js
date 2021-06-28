cef7e218a245a26477c13c6231203a9b89c6e2eb
errengonzalez <78234978+errengonzalez@users.noreply.github.com>
2021-06-28 20:53:11 -0300

    Add files via upload
    primer commit

---
+++ jsEntradaSalida-01.js
@@ -0,0 +1,7 @@
+/*
+Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+function mostrar() {
+	alert("chau mundo");//va a mostrar otra ventana con este mensaje
+}
+
