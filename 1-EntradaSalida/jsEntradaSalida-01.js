e4a1835d5a3838c6d229d29f3901d54de231fb06
Your Name <you@example.com>
2021-06-28 20:09:26 -0300

    estado normal

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,6 +2,6 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-	alert("hola munde");//va a mostrar otra ventana con este mensaje
+	alert("okey");//va a mostrar otra ventana con este mensaje
 }
 
