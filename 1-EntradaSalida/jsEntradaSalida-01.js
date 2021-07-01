1e7b753cae49ff6fee94e2afc5e4bce27b40f737
Your Name <you@example.com>
2021-07-01 19:08:24 -0300

    segundo commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,6 +2,6 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-	alert("okey");//va a mostrar otra ventana con este mensaje
+	alert("bienvenidos division a");//va a mostrar otra ventana con este mensaje
 }
 
